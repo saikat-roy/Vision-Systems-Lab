7d5a78127787db39b8758d042e64a3ec23ca2728
saikat-roy <saikatroy@uni-bonn>
2019-05-01 13:13:18 +0200

    Improved softmax non-linearity

--- Project1/softmax_regression.py
+++ Project1/softmax_regression.py
@@ -12,6 +12,14 @@ class SoftmaxRegression:
         self.W = np.random.rand(k, n_class)
         self.b = np.zeros((1, n_class))
 
+    def softmax(self, x):
+        """
+        Softmax Nonlinearity applied to x and returned. Does not do argmax
+        :param x: 1 x k ndarray
+        :return: softmax non-linearity applied to x
+        """
+        return np.exp(x) / np.sum(np.exp(x))
+
     def fit(self, x, y):
         """
         Fit the softmax regression model to data. Final usage as classification or regression model irrelevant here.
@@ -26,19 +34,18 @@ class SoftmaxRegression:
 
         :param X: n x k ndarray for prediction
         :param probs: True to return probs
+        :return: Softmax activations or class predictions, depending on probs parameter.
         """
         y = np.dot(x,self.W)+self.b
         print(y)
-        softmax = lambda a: a/np.sum(a)
         if probs:
-            return np.apply_along_axis(softmax, 1, y)
+            return np.apply_along_axis(self.softmax, 1, y)
         else:
-            return np.argmax(np.apply_along_axis(softmax, 1, y), axis=1)
-
+            return np.argmax(np.apply_along_axis(self.softmax, 1, y), axis=1)
 
 if __name__ == "__main__":
 
     model = SoftmaxRegression(3, 2)
     a = np.array([[1, 2, 3], [5, 6, 7]])
 
-    print(model.predict(a))
+    print(model.predict(a, True))
