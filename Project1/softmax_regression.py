9ac692ab24b2bf006484ac6ffb08e14a70f78ad6
saikat-roy <saikatroy@uni-bonn>
2019-05-01 02:36:10 +0200

    Finish basic structure of softmax regression. Yet to write fit(..)

---
+++ Project1/softmax_regression.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+class SoftmaxRegression:
+
+    def __init__(self, k, n_class):
+        """
+        Constructor for object initialization
+
+        :param k: number of dimensions of the data
+        :param n_class: number of classes/output variables for the problem
+        """
+        self.W = np.random.rand(k, n_class)
+        self.b = np.zeros((1, n_class))
+
+    def fit(self, x, y):
+        """
+        Fit the softmax regression model to data. Final usage as classification or regression model irrelevant here.
+
+        :param x: n x k ndarray of data points for model training/fitting
+        :param y: n x n_class ndarray of data points for fitting
+        """
+
+    def predict(self, x, probs = False):
+        """
+        Predicts the softmax regression output as classification or regression outputs
+
+        :param X: n x k ndarray for prediction
+        :param probs: True to return probs
+        """
+        y = np.dot(x,self.W)+self.b
+        print(y)
+        softmax = lambda a: a/np.sum(a)
+        if probs:
+            return np.apply_along_axis(softmax, 1, y)
+        else:
+            return np.argmax(np.apply_along_axis(softmax, 1, y), axis=1)
+
+
+if __name__ == "__main__":
+
+    model = SoftmaxRegression(3, 2)
+    a = np.array([[1, 2, 3], [5, 6, 7]])
+
+    print(model.predict(a))
