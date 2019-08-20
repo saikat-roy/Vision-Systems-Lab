865279019d255443dafcbb416235dd8dbca164b2
saikat-roy <saikatroy@uni-bonn>
2019-08-20 20:17:41 +0200

    Finished Dataloader class

--- Project9/utils.py
+++ Project9/utils.py
@@ -4,31 +4,90 @@ import os
 import numpy as np
 
 import cv2
+import torch
 from torch.utils.data import Dataset, DataLoader
-
 
 class CudaVisionDataset(Dataset):
 
-    def __init__(self, dir_path):
+    def __init__(self, dir_path, no_of_classes=4, channel_lut=None, blob_rad = 5):
+        """
 
+        :param dir_path:
+        :param no_of_classes:
+        :param channel_lut:
+        :param blob_rad: Try to keep odd
+        """
         super(CudaVisionDataset, self).__init__()
         self.img_paths, self.annot_paths = read_files(img_dir_path = dir_path)
+        self.no_of_classes = no_of_classes
+        if channel_lut is None:
+            channel_lut = {'Head':0, 'Hand':1, 'Leg':2, 'Trunk':3}
+        self.channel_lut = channel_lut
+        self.blob_rad = blob_rad
 
     def __getitem__(self, index):
         #print(index)
         img = cv2.imread(self.img_paths[index],1)
-        print(img.shape) # Shape is l x w x c
-        cv2.imshow('image', img)
-        cv2.waitKey()
+        # print(img.shape) # Shape is l x w x c
+        l,w,c = img.shape
+        # cv2.imshow('image', img)
+        # cv2.waitKey()
 
         annot = parse_annotations(self.annot_paths[index])
-        exit()
-        return img, annot
+
+        targets = np.zeros((self.no_of_classes, l, w), dtype='float')
+
+        for k in annot.keys():
+            # print(annot[k])
+            for p in annot[k]:
+                targets[self.channel_lut[k], int(p[0]), int(p[1])] = 1.0
+
+        # print(targets.shape)
+        targets = torch.Tensor(targets)
+        downsampler = torch.nn.MaxPool2d(kernel_size=4, stride=4, padding=0)
+        targets = downsampler(targets).numpy()
+        # print(targets.shape)
+
+        gaussian_2d = define_2d_gaussian(rad=self.blob_rad)
+        target_coords_downsampled = np.where(targets==1)
+        # print(np.where(targets==1))
+        for i in range(target_coords_downsampled[0].shape[0]):
+            targets[target_coords_downsampled[0][i]] = centre_and_place(targets[target_coords_downsampled[0][i]],
+                                                    gaussian_2d, self.blob_rad, (target_coords_downsampled[1][i],
+                                                    target_coords_downsampled[2][i]))
+
+        img = np.moveaxis(img,2,0) # cv2 images are l X w X c
+
+        img = torch.Tensor(img)
+        targets = torch.Tensor(targets)
+
+        return img, targets
 
 
     def __len__(self):
-        return self.len
+        return len(self.img_paths)
 
+
+def centre_and_place(arr, g, rad, coords):
+
+    lt = int(rad/2)
+    rt = int(rad/2)+1
+    if rad%2==0:
+        rt -= 1
+    # print(max(0,coords[0]-lt), min(arr.shape[0],coords[0]+rt),
+    #     max(0,coords[1]-lt), min(arr.shape[1],coords[1]+rt))
+    arr[max(0,coords[0]-lt): min(arr.shape[0],coords[0]+rt),
+        max(0,coords[1]-lt): min(arr.shape[1],coords[1]+rt)] = g
+
+    return arr
+
+def define_2d_gaussian(rad=5, mu=0.0, sigma=8.0):
+
+    x, y = np.meshgrid(np.linspace(-1, 1, rad), np.linspace(-1, 1, rad))
+    d = np.sqrt(x * x + y * y)
+    sigma, mu = 1.0, 0.0
+    g = np.exp(-((d - mu) ** 2 / (2.0 * sigma ** 2)))
+    return g
 
 def parse_annotations(fname):
     with open(fname) as f:
@@ -37,18 +96,19 @@ def parse_annotations(fname):
     data = data['shapes']
     dpoints = {'Head':np.array([]), 'Hand': np.array([]), 'Leg': np.array([]), 'Trunk': np.array([])}
     for d in data:
-        print(d)
+        # print(d)
         label = d['label']
         bbox_coords = np.array(d['points'])
         mid = (bbox_coords[0]+bbox_coords[1])/2
-        print(bbox_coords, mid)
-        dpoints[label] = np.append(dpoints[label], mid, axis=0)
+        # print(bbox_coords, mid)
+        dpoints[label] = np.append(dpoints[label], mid[::-1], axis=0)
 
     for k in dpoints.keys():
         dpoints[k] = dpoints[k].reshape((-1,2))
         # print(bbox_coords)
         # points
-    print(dpoints)
+    # print(dpoints)
+    return dpoints
 
 
 def read_files(img_dir_path, img_format='.jpg', annot_format='.json', annot_folder_name='json'):
@@ -88,4 +148,4 @@ if __name__ == "__main__":
 
     dataset = CudaVisionDataset('./data/Images')
     for i in enumerate(dataset):
-        0
+        print(0)
